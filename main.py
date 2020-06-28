0fc09414fbf4c3dd09097edd1a0856bbe2bac32c
IHYPGC <james.corbett59@gmail.com>
2020-06-28 18:34:10 +0930

    Update main.py
    Modified code to compress images to zip

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import requests
 import ctypes
 import sys
 import os
+import zipfile
+import shutil
 ctypes.windll.kernel32.SetConsoleTitleW("JSMNN's Yiffer.XYZ Downloader")
 
 print("If you want to download more comics than one, please put a ', ' between them.")
@@ -14,6 +16,8 @@ splittedinput, comicnameholder = [], []
 ## VARIABLES
 INDEX_URL    = "https://yiffer.xyz/"
 COMICS_URL   = INDEX_URL + "comics/"
+
+CBZ = 1
 
 ## MAIN CODE
 if INDEX_URL in websiteURL:
@@ -25,7 +29,14 @@ if INDEX_URL in websiteURL:
     for i in comicnameholder:
         A_02          = 1                      						## USED FOR LOOPING FILES (01.jpg, 02.jpg, numbers are A_02)
         A_02B         = True                   						## USED FOR WHILE LOOP :DDDD, IF THERE'S AN ERROR (DATA.status_code != requests.codes.ok)
-        DirectoryPath = sys.path[0] + "\\" + i.replace("%20", " ")  ## EXPLAINS ITSELF.
+        
+        if CBZ == 1:
+            DirectoryPath = "Download"
+        else:
+            DirectoryPath = sys.path[0] + "\\" + i.replace("%20", " ")  ## EXPLAINS ITSELF.            
+        
+        DirectoryPath2 = sys.path[0] + "\\" + i.replace("%20", " ")  ## EXPLAINS ITSELF.            
+
         if not os.path.exists(DirectoryPath):
             os.mkdir(DirectoryPath)
             print("[Created Directory for Comic]: " + i.replace("%20", " "))
@@ -43,5 +54,14 @@ if INDEX_URL in websiteURL:
                 else:
                     print("[Done Downloading]: " + i)
                     A_02B = False
+
+        if CBZ == 1:
+            comic_zip = zipfile.ZipFile(DirectoryPath2+'.cbz', 'w')
+            for folder, subfolders, files in os.walk(DirectoryPath):
+                for file in files:
+                    comic_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder,file), DirectoryPath), compress_type = zipfile.ZIP_DEFLATED)
+            comic_zip.close()
+            shutil.rmtree(DirectoryPath, ignore_errors=False, onerror=None)
+
         else:
             print("[Directory exists]: " + DirectoryPath)
